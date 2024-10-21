e604f5aa265e45908f24d492c978a7381b6aec34
Bernardo Giudicelli <giudicelli.be@gmail.com>
2024-10-21 14:13:30 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { QuemSomosComponent } from './componentes/quem-somos/quem-somos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,HomeComponent,QuemSomosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { QuemSomosComponent } from './componentes/quem-somos/quem-somos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '' , component: HomeComponent},
+    {path: 'quem-somos', component:QuemSomosComponent}
+];
